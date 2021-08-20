4ccd97115f58f3692e852ce3b06b92884e62c8ec
WrangkanaJK <87839864+WrangkanaJK@users.noreply.github.com>
2021-08-20 10:22:07 +0700

    Update app.py

--- app.py
+++ app.py
@@ -34,11 +34,16 @@ def get_api():
 def name():
         return " <font color= brown>วรางคณา นุชเกษม</font> <br>เลขที่ 18 ม.4/10"
 
+
 @app.route('/hello/<string:name>')
 
 def Home(name):
 
 	return render_template('home.html', name_html=name)
+
+
+
+
 if __name__ == "__main__":
     app.run(debug=False)
 
